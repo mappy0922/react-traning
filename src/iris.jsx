e8c50d4a9cca86a38a58f95ca43bac7e2a7585d0
LAPTOP-SSDSJOVC\masah <mr.masa0329@gmail.com>
2026-05-21 21:48:21 +0900

    iris作業中

--- src/iris.jsx
+++ src/iris.jsx
@@ -1,32 +1,80 @@
+import { blurImage, reduce } from "d3";
 import { iris_data } from "./iris_data";
 import { useState } from "react";
 
-const xproperty = [
+const property = [
     "sepalLength",
     "sepalWidth",
     "petalLength",
     "petalWidth"
 ];
-const yproperty = [
-    "sepalLength",
-    "sepalWidth",
-    "petalLength",
-    "petalWidth"
- ];
+
+const dataColor = {
+    "setosa": "lightgreen",
+    "versicolor": "plum",
+    "virginica": "lightsalmon",
+};
 
 export default function App() {
     const width = 800;
     const height = 800;
-    const [selected_X, setselected_X] = useState(xproperty[0]);
-    const [selcted_Y, setselected_Y] = useState(yproperty[1]);
+    const [selected_X, setselected_X] = useState(property[0]);
+    const [selcted_Y, setselected_Y] = useState(property[1]);
 
     return (
-        <svg width = { width } height = { height }>
-            <g>
-                {iris_data.map((item, i) => (
-                    <circle key={i} cx={item[selected_X] * 80} cy={height - item[selcted_Y] * 80} r="5" />
-                ))} 
-            </g>
-        </svg>
+        <div className = "top">
+            <h1 className="title">scatter plot of iris data</h1>
+
+            <div className="controls">
+
+                <label>
+                    X :
+                    <select
+                        value={selected_X}
+                        onChange={(e) =>
+                            setselected_X(e.target.value)
+                        }
+                    >
+                        {property.map((p) => (
+                            <option key={p} value={p}>
+                                {p}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+
+                <label>
+                    Y :
+                    <select
+                        value={selcted_Y}
+                        onChange={(e) =>
+                            setselected_Y(e.target.value)
+                        }
+                    >
+                        {property.map((p) => (
+                            <option key={p} value={p}>
+                                {p}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+
+            </div>
+
+            <div className="chart">
+                <svg width = { width } height = { height }>
+                    <g>
+                        {iris_data.map((item, i) => (
+                            <circle key={i} 
+                            cx={item[selected_X] * 80} 
+                            cy={height - item[selcted_Y] * 80} 
+                            r="5" 
+                            fill={dataColor[item.species]} 
+                            />
+                        ))} 
+                    </g>
+                </svg>
+            </div>
+        </div>
     )
 }
